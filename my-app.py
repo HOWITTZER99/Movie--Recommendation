5c3522246083222aa9f6d233e86488fae6457346
HOWITTZER99 <77449343+HOWITTZER99@users.noreply.github.com>
2023-03-28 15:53:41 +0530

    Create my-app.py

---
+++ my-app.py
@@ -0,0 +1,33 @@
+import json
+import pandas as pd
+import streamlit as st
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+import json
+
+with open('movie.json', 'r') as f:
+    movie_dict = json.load(f)
+movies = pd.DataFrame(movie_dict)
+cv = CountVectorizer(max_features=5000, stop_words='english')
+vectors = cv.fit_transform(movies['tags']).toarray()
+similar = cosine_similarity(vectors)
+
+def recommend_content(movie):
+    movie_index = movies[movies['title'] == movie].index[0]
+    dist = similar[movie_index]
+    movie_list = sorted(list(enumerate(dist)), reverse=True, key=lambda x: x[1])[1:6]
+    movie_name = []
+    for i in movie_list:
+        movie_name.append(movies.iloc[i[0]].title)
+    return movie_name
+
+st.title('Movie Recommender')
+
+with st.form(key='movie_form'):
+    movie_input = st.text_input('Enter a movie title')
+    submit_button = st.form_submit_button(label='Recommend')
+
+if submit_button:
+    recommended_movies = recommend_content(movie_input)
+    st.write('Recommended movies:')
+    st.write(json.dumps(recommended_movies))
